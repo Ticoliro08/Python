d4e3982b123e46f7a2cafe2de3098f3132e148ad
Guilherme <ggsantana18@gmail.com>
2025-01-30 19:39:00 -0300

    Add files via upload

---
+++ calculadoraWeb.py
@@ -0,0 +1,35 @@
+import streamlit as st
+
+
+st.title('CALCULADORA')
+st.divider()
+
+sinal = st.text_input("Ola, que tipo de conta deseja fazer? multiplicação, divisão, soma, subtração. Digite o tipo de conta desejada: ")
+a = st.number_input('Digite o primeiro numero:\n ')
+b = st.number_input('Digite o segundo numero:\n ')
+
+
+
+if sinal == "multiplicação":
+  conta  = a * b
+  st.write(f"O resultado de sua conta de multiplicação é de {conta}.")
+
+elif sinal =="divisão":
+ conta  = a / b
+        
+ st.write(f"O resultado de sua conta de divisão é de {conta}.")
+ 
+elif sinal =="soma":
+ conta  = a + b
+        
+ st.write(f"O resultado de sua conta de soma é de {conta}.")
+
+elif sinal == "subtração":
+ conta  = a - b
+        
+ st.write(f"O resultado de sua conta de subtração é de {conta}.")
+     
+else:
+ st.write("Opção não encontrada!")
+    
+  
